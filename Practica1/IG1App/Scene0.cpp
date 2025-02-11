15fc416495ebfc161e9ce8b9332c1dc34daf70ed
TelmoK <telmo.kiss@gmail.com>
2025-02-11 20:42:22 +0100

    Apartado 11

--- Practica1/IG1App/Scene0.cpp
+++ Practica1/IG1App/Scene0.cpp
@@ -1,4 +1,5 @@
 #include "Scene0.h"
+#include <glm/gtc/matrix_transform.hpp>
 
 void Scene0::init()
 {
@@ -12,4 +13,10 @@ void Scene0::init()
 	gObjects.push_back(new RGBAxes(400.0));
 	gObjects.push_back(new RegularPolygon(3, 100, glm::dvec4(0, 255, 255, 1)));
 	gObjects.push_back(new RegularPolygon(100, 100, glm::dvec4(255, 0, 255, 1)));
+	
+	RGBTriangle* rgbTriangle = new RGBTriangle(20);
+
+	rgbTriangle->setModelMat(glm::translate(rgbTriangle->modelMat(), {100, 0, 0}));
+
+	gObjects.push_back(rgbTriangle);
 }
