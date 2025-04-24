edf6dc2f4335bd7d6c719a200ba97420a34861af
TelmoK <telmo.kiss@gmail.com>
2025-04-24 21:06:22 +0200

    Clearing

--- Practica4/IG1App/Scene8.cpp
+++ Practica4/IG1App/Scene8.cpp
@@ -6,14 +6,7 @@ void Scene8::init()
 
 	gObjects.push_back(new RGBAxes(400.0));
 
-	gObjects.push_back(new Sphere(200, 40, 40));
-	// gObjects.push_back(new Toros(200, 50));
-	//gObjects.push_back(new Disk(50, 250, 5, 5));
-	//gObjects.push_back(new Cone(300, 250, 0, 5, 5));
-	//gObjects.push_back(new WingAdvancedTIE());
-	//gObjects.push_back(new TieFighter());
-
-	//gObjects.push_back(new IndexedBox(300));
+	gObjects.push_back(new IndexedBox(300));
 }
 
 void Scene8::showNormals()
